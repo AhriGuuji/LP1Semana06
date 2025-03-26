d848cdbbfcf95a9997cf52ff0d65208ca0e10d61
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-26 15:24:15 +0000

    Add project Chronos

---
+++ Chronos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch chrono1 = new Stopwatch();
+            Stopwatch chrono2 = new Stopwatch();
+        }
+    }
+}
