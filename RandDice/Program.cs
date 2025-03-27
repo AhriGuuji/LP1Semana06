073a063206ff401ba4f93b3c68a8796377c96abb
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-27 17:35:00 +0000

    Fixed why is not giving the same number as the powerpoint

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -7,10 +7,10 @@ namespace RandDice
         private static void Main(string[] args)
         {
             int total = 0;
+            Random value = new Random(int.Parse(args[1]));
             for (int i = 0; i < int.Parse(args[0]); i++)
             {
-                Random value = new Random(int.Parse(args[1]));
-                total += value.Next(0,7);
+                total += value.Next(1,7);
             }
             Console.WriteLine(total);
         }
