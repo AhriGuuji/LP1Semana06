0cfaf7e0172b9412bba283e81b190d48498c75f5
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-27 17:27:16 +0000

    Added the sum of the dices

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,10 +6,13 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
+            int total = 0;
             for (int i = 0; i < int.Parse(args[0]); i++)
             {
-                
+                Random value = new Random(int.Parse(args[1]));
+                total += value.Next(0,7);
             }
+            Console.WriteLine(total);
         }
     }
 }
