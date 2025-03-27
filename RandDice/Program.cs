c8c037aaf0961b0bed2858746f7c57840162d6e9
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-27 17:02:46 +0000

    Add RandDice

---
+++ RandDice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                
+            }
+        }
+    }
+}
