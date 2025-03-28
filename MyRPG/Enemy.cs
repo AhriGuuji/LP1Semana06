3044704e18879dbe5a6c5cfe167ec219b248c44a
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-28 14:03:08 +0000

    Add MyRPG

---
+++ MyRPG/Enemy.cs
@@ -0,0 +1,77 @@
+using System;
+
+namespace MyRPG
+{
+    public class Enemy
+    {
+        private string name;
+        private float health;
+        private float shield;
+        private static int quantityPU;
+
+        static Enemy()
+        {
+            quantityPU = 0;
+        }
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            quantityPU = 0;
+            health = 100;
+            shield = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public void SetName(string newName)
+        {
+            if (newName.Length > 8) name = newName.Substring(0,8);
+            else name = newName;
+        } 
+
+        public void PickupPowerUp(PowerUp powerUp,float amount)
+        {
+            if (powerUp == PowerUp.Health)
+            {
+                health += amount;
+                if (health > 100) {health = 100;}
+            }
+            if (powerUp == PowerUp.Shield)
+            {
+                shield += amount;
+                if (shield > 100) {shield = 100;}
+            }
+            quantityPU++;
+        }
+
+        public static int HowManyPowerUps()
+        {
+            return quantityPU;
+        }
+    }
+}
